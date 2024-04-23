59cfa6ce2132e55eec5ce006a0fe0a8d3ab89125
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-04-23 22:48:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatterbox/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.chatterbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.chatterbox.ui.theme.ChatterBoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatterBoxTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
