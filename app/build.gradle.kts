fc0e062478744f65bcaabff9cd7e392e000ff3e3
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-06-13 21:09:40 +0530

    finished almost

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,8 +135,6 @@ dependencies {
     // Compose Navigation
     implementation ("androidx.navigation:navigation-compose:2.8.0-beta01")
 
-
-
     implementation ("androidx.compose.runtime:runtime-livedata:1.7.0-beta01")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
 
